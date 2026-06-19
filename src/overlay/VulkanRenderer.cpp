1510ad32d601bfa2d9176214dfbb44b50bd368bf
Nyabsi <nyabsi@sovellus.cc>
2026-06-19 20:43:43 +0300

    fix: make it compile with latest ImGui

--- src/overlay/VulkanRenderer.cpp
+++ src/overlay/VulkanRenderer.cpp
@@ -219,18 +219,22 @@ auto VulkanRenderer::Initialize()  -> void
     vkGetDeviceQueue(vulkan_device_, vulkan_queue_family_, 0, &vulkan_queue_);
 
     VkDescriptorPoolSize pool_sizes[] = {
-        {
-            VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-            IMGUI_IMPL_VULKAN_MINIMUM_IMAGE_SAMPLER_POOL_SIZE
-        },
+         { VK_DESCRIPTOR_TYPE_SAMPLER,                128 },
+         { VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 512 },
+         { VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE,          128 },
+         { VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,          128 },
+         { VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER,   64  },
+         { VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER,   64  },
+         { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,         256 },
+         { VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,         256 },
     };
 
-    VkDescriptorPoolCreateInfo pool_info =
-    {
-        .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO,
-        .flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT,
-        .maxSets = 0,
-    };
+    VkDescriptorPoolCreateInfo pool_info = {};
+    pool_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    pool_info.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
+    pool_info.maxSets = 2048;
+    pool_info.poolSizeCount = std::size(pool_sizes);
+    pool_info.pPoolSizes = pool_sizes;
 
     for (VkDescriptorPoolSize& pool_size : pool_sizes)
         pool_info.maxSets += pool_size.descriptorCount;
